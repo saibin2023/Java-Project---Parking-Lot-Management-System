d4cb55d8b0006fcefab22a6b9d37aa41b5406475
saibin2023 <127604103+saibin2023@users.noreply.github.com>
2025-02-14 17:36:29 +0900

    Update ParkingLot.java

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -10,6 +10,7 @@ List<Slot> 表示一个停车位列表，每个列表包含一个楼层的所有
 List<List<Slot>> 表示一个包含多个楼层停车位列表的列表，即整个停车场的所有停车位。
 具体来说，每个 List<Slot> 对应一个楼层，而 slots 则包含了所有楼层的停车位列表。
 */
+    //该构造函数将根据指定的楼层数和每层的停车位数量，初始化一个包含不同类型停车位的停车场对象
     ParkingLot(String parkingLotId, int nfloors, int noOfSlotsPerFlr) {
         this.parkingLotId = parkingLotId;
         slots = new ArrayList<>();//初始化一个新的 ArrayList，用于存储每层楼的停车位
@@ -25,7 +26,7 @@ List<List<Slot>> 表示一个包含多个楼层停车位列表的列表，即整
             }
         }
     }
-
+    //parkVehicle此方法的作用是根据车辆类型在停车场中找到一个空闲的合适停车位，并返回停车票据ID。如果没有找到合适的停车位，则返回 null。
     public String parkVehicle(String type, String regNo, String color) {
         Vehicle vehicle = new Vehicle(type, regNo, color);
         for (int i = 0; i < slots.size(); i++) { //外层循环，遍历每层楼的停车位列表。
@@ -41,35 +42,41 @@ List<List<Slot>> 表示一个包含多个楼层停车位列表的列表，即整
         System.out.println("NO slot available for given type"); //没有找到合适的停车位
         return null;
     }
+    
+    //这个方法的作用是为每个停车操作生成一个唯一的票据ID，以便后续查找和管理
     private String generateTicketId(int flr, int slno){ //通过 generateTicketId(i + 1, j + 1) 生成停车票据ID
         return parkingLotId + "_" + flr + "_" + slno;
     }
+    //unPark，getNoOfOpenSlots两个方法结合起来，实现了停车场中车辆的移除和空闲停车位数量的统计功能
     public void unPark(String ticketId){
-        String[] extract = ticketId.split("_");
-        int flr_idx=Integer.parseInt(extract[1])-1;
-        int slot_idx=Integer.parseInt(extract[2])-1;
+        String[] extract = ticketId.split("_");//将票据ID按下划线 _ 分割成数组
+        int flr_idx=Integer.parseInt(extract[1])-1;//从票据ID中提取楼层索引，并将其转换为整数（从1基数改为0基数）
+        int slot_idx=Integer.parseInt(extract[2])-1;//从票据ID中提取停车位索引，并将其转换为整数（从1基数改为0基数）
         for(int i=0; i<slots.size();i++){
-            for(int j=0;j<slots.get(i).size(); j++){
+            for(int j=0;j<slots.get(i).size(); j++){ //嵌套循环遍历所有楼层和每层的停车位，找到与提取的楼层和停车位索引匹配的停车位
                 if(i==flr_idx && j==slot_idx) {
                     Slot slot = slots.get(i).get(j);
                     slot.vehicle=null;
                     slot.ticketId=null;
-                    System.out.println("Unparked vehicle");
+                    System.out.println("Unparked vehicle"); //将匹配的停车位的 vehicle 和 ticketId 置为空，并输出 "Unparked vehicle"
                 }
             }
         }
     }
+    
     int getNoOfOpenSlots(String type){
-        int count=0;
-        for(List<Slot> floor: slots){
-            for(Slot slot: floor){
-                if(slot.vehicle == null && slot.type.equals(type)) count++;
+        int count=0; //初始化计数器
+        for(List<Slot> floor: slots){ //外层循环遍历所有楼层的停车位列表
+            for(Slot slot: floor){ //内层循环遍历每层楼的停车位
+                if(slot.vehicle == null && slot.type.equals(type)) count++; //检查停车位是否为空并且类型匹配，若满足条件则计数器加1
             }
         }
 
-        return count;
+        return count;//返回计数器的值，即特定类型的空闲停车位数量
     }
-    void displayOpenSlots(String type){
+    
+    //displayOpenSlots，displayOccupiedSlots两个方法通过遍历 slots 列表，分别输出特定类型的空闲和已占用停车位信息
+    void displayOpenSlots(String type){ //遍历所有楼层和每层楼的停车位，满足条件，输出该停车位所在的楼层和编号
         for(int i=0;i<slots.size();i++){
             for(int j=0;j<slots.get(i).size();j++){
                 Slot slot=slots.get(i).get(j);
